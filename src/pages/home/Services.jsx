1f60ad2e20774cf8369f41b5b8b4f3ffd1b04f7c
comet-syt <imsut2713@gmail.com>
2023-01-15 20:30:34 +0900

    create_mainpages #1
    1. created functional components(header, banner, news, activities, services, supports) with css

---
+++ src/pages/home/Services.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div className="contents_section contents-area">
+      <h1>
+        SERVICES
+        <a href="home" target="_self">
+          더보기(more)
+        </a>
+      </h1>
+      <h2>Hexa에서 제공하는 서비스입니다</h2>
+    </div>
+  );
+}
+
+export default Services;
