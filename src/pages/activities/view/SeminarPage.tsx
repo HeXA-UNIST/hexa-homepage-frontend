29ba1a7400246e9badda7f742a7000f58d4a519b
chamwhy <chamwhy8@gmail.com>
2023-09-24 02:17:13 +0900

    Update SeminarPage.tsx

--- src/pages/activities/view/SeminarPage.tsx
+++ src/pages/activities/view/SeminarPage.tsx
@@ -7,74 +7,76 @@ import SeminarPageViewModel from "@pages/activities/vm/seminar_page_view_model";
 import SeminarCard from "@pages/activities/components/SeminarCard";
 
 function TitlePart() {
-  return (
-    <div className="seminar-page__title">
-      <h2>HeXA가 진행한 세미나</h2>
-      <span>HeXA는, 지식을 나누기 위해 정기적으로 스터디를 진행합니다.</span>
-    </div>
-  );
+    return (
+        <div className="seminar-page__title">
+            <h2>HeXA가 진행한 세미나</h2>
+            <span>
+                HeXA는, 지식을 나누기 위해 정기적으로 스터디를 진행합니다.
+            </span>
+        </div>
+    );
 }
 
 const QueryFormPart = observer(
     ({
-      seminarPageViewModel,
+        seminarPageViewModel,
     }: {
-      seminarPageViewModel: SeminarPageViewModel;
+        seminarPageViewModel: SeminarPageViewModel;
     }) => {
-      const { seminarQueryOptions } = seminarPageViewModel;
-  
-      return (
-        <div className="seminar-page__query-form">
-          <div>
-            <SearchBox
-              style={{
-                width: "100%",
-              }}
-              value={seminarQueryOptions.searchText}
-              onChange={(text) => {
-                seminarPageViewModel.setSearchText(text);
-                seminarPageViewModel.fetchSeminars();
-              }}
-              placeholder="검색 (예: BUS HeXA, tag:서비스)"
-            />
-          </div>
-        </div>
-      );
+        const { seminarQueryOptions } = seminarPageViewModel;
+
+        return (
+            <div className="seminar-page__query-form">
+                <div>
+                    <SearchBox
+                        style={{
+                            width: "100%",
+                        }}
+                        value={seminarQueryOptions.searchText}
+                        onChange={(text) => {
+                            seminarPageViewModel.setSearchText(text);
+                            seminarPageViewModel.fetchSeminars();
+                        }}
+                        placeholder="검색 (예: BUS HeXA, tag:서비스)"
+                    />
+                </div>
+            </div>
+        );
     }
-  );
-  
-  const SeminarListPart = observer(
+);
+
+const SeminarListPart = observer(
     ({
-      seminarPageViewModel,
+        seminarPageViewModel,
     }: {
-      seminarPageViewModel: SeminarPageViewModel;
+        seminarPageViewModel: SeminarPageViewModel;
     }) => {
-      const { queryResult } = seminarPageViewModel;
-        
-      return (
-        <div className="seminar-page__seminar-list">
-          {queryResult.seminars.map((seminar) => (
-            <SeminarCard key={seminar.seminarId} seminar={seminar} />
-          ))}
-        </div>
-      );
+        const { queryResult } = seminarPageViewModel;
+
+        return (
+            <div className="seminar-page__seminar-list">
+                {queryResult.seminars.map((seminar) => (
+                    <SeminarCard key={seminar.seminarId} seminar={seminar} />
+                ))}
+            </div>
+        );
     }
-  );
+);
 
 function SeminarPage({
     seminarPageViewModel,
-  }: {
+}: {
     seminarPageViewModel: SeminarPageViewModel;
-  }) {
+}) {
     seminarPageViewModel.fetchSeminars();
-    
+
     return (
-      <ContentArea>
-        <TitlePart />
-        <QueryFormPart seminarPageViewModel={seminarPageViewModel} />
-        <SeminarListPart seminarPageViewModel={seminarPageViewModel} />
-      </ContentArea>
+        <ContentArea>
+            <TitlePart />
+            <QueryFormPart seminarPageViewModel={seminarPageViewModel} />
+            <SeminarListPart seminarPageViewModel={seminarPageViewModel} />
+        </ContentArea>
     );
-  }
+}
 
 export default SeminarPage;
