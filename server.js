d78c0d1bf9f14707cbed212dfc3ef017081cedc8
chamwhy <chamwhy8@gmail.com>
2024-05-28 22:49:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('*', (req, res) => {
     }
   });
 
+  app.get('/activity', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
