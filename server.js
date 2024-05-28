1242d2b9120cacaa7472728a13ff4ea9ec2f9b22
chamwhy <chamwhy8@gmail.com>
2024-05-28 23:16:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const fs = require("fs");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
-app.use(express.static(path.join(__dirname, 'build/static')));
 
 app.get("*", (req, res) => {
     const filePath = path.join(__dirname, "build", req.url);
-    console.log("request", req.url);
     if (fs.existsSync(filePath) && !fs.lstatSync(filePath).isDirectory()) {
         res.sendFile(filePath);
     } else {
