f29963c6854c014cb39640ccfc2beab885b6b4ee
chamwhy <chamwhy8@gmail.com>
2024-05-28 22:38:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get('*', (req, res) => {
+    const filePath = path.join(__dirname, 'build', req.url);
+    if (fs.existsSync(filePath) && !fs.lstatSync(filePath).isDirectory()) {
+      res.sendFile(filePath);
+    } else {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
