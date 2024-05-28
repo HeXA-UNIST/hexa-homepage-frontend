e0c1b6c9a20d4ca733396c558cc3fdb033694dad
chamwhy <chamwhy8@gmail.com>
2024-05-28 20:34:05 +0900

    feat: 정리

--- src/common/components/DetailItem.tsx
+++ src/common/components/DetailItem.tsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { Image } from "./ImageContent";
+import { IAttachment } from "@models/seminar/Seminar";
+import { Image, FileDownload } from "./ImageContent";
 
 function DetailItem({
     returnLink,
@@ -20,9 +21,10 @@ function DetailItem({
     image: number | undefined;
     contentTitle: string;
     content: string;
-    attachments: number[] | undefined;
+    attachments: IAttachment[] | undefined;
     children: React.ReactNode;
 }) {
+    console.log(attachments);
     return (
         <div className="grid gap-x-12 gap-y-6 grid-cols-[20rem_minmax(20rem,_1fr)] grid-rows-[3rem_min-content_min-content_auto]">
             <div className="text-left col-start-1 col-end-2 row-start-1 row-end-2 rounded-md">
@@ -56,18 +58,20 @@ function DetailItem({
             </div>
             {attachments !== undefined && (
                 <div
-                className="col-start-1 col-end-2 row-start-4 row-end-6"
+                className="col-start-1 col-end-2 row-start-4 row-end-6 rounded-2xl p-4 bg-zinc-900 text-white flex flex-col gap-4"
             >
-                {children}
+                {attachments.map(at => (
+                   <FileDownload attachment={at} className="bg-zinc-800 rounded-xl p-2 text-left px-4"/>
+                ))}
             </div>
             )
 
             }
-            <div className="col-start-2 col-end-3 row-start-2 row-end-6 bg-zinc-900 rounded-2xl p-8 text-white">
+            <div className="col-start-2 col-end-3 row-start-2 row-end-6 bg-zinc-900 rounded-2xl p-8 text-white flex flex-col">
                 <div className=" px-4 mb-3 text-left text-xl">
                     {contentTitle}
                 </div>
-                <div className=" h-fit p-6 bg-zinc-800 rounded-2xl text-left break-words  w-full">
+                <div className=" h-fit p-6 bg-zinc-800 rounded-2xl text-left break-words  w-full grow">
                     <div className="break-words whitespace-pre-wrap w-full">{content}</div>
                 </div>
             </div>
